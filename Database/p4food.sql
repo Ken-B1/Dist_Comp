c3270f78003c5742e242bf1df4e63a911220db44
KenB <kenbauwens@hotmail.com>
2018-04-21 14:08:49 +0200

    Added tables for boards and categories followed by users

--- Database/p4food.sql
+++ Database/p4food.sql
@@ -47,6 +47,62 @@ INSERT INTO `account` VALUES (1,1,'admin','admin','admin','admin','admin','admin
 UNLOCK TABLES;
 
 --
+-- Table structure for table `accountcategoryjunction`
+--
+
+DROP TABLE IF EXISTS `accountcategoryjunction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `accountcategoryjunction` (
+  `accountid` int(11) NOT NULL,
+  `categoryid` int(11) NOT NULL,
+  PRIMARY KEY (`accountid`,`categoryid`),
+  KEY `id_idx` (`categoryid`),
+  CONSTRAINT `FK_ACCOUNT_ID` FOREIGN KEY (`accountid`) REFERENCES `account` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_CATEGORY_ID` FOREIGN KEY (`categoryid`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `accountcategoryjunction`
+--
+
+LOCK TABLES `accountcategoryjunction` WRITE;
+/*!40000 ALTER TABLE `accountcategoryjunction` DISABLE KEYS */;
+/*!40000 ALTER TABLE `accountcategoryjunction` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `board`
+--
+
+DROP TABLE IF EXISTS `board`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `board` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `boardname` varchar(45) NOT NULL,
+  `category` int(11) DEFAULT NULL,
+  `owner` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `FK_OWNER_ID_idx` (`owner`),
+  KEY `FK_CATEGORY_ID_idx` (`category`),
+  CONSTRAINT `FK_BOARDACCOUNT_ID` FOREIGN KEY (`owner`) REFERENCES `account` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_BOARDCATEGORY_ID` FOREIGN KEY (`category`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `board`
+--
+
+LOCK TABLES `board` WRITE;
+/*!40000 ALTER TABLE `board` DISABLE KEYS */;
+/*!40000 ALTER TABLE `board` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `categories`
 --
 
@@ -59,7 +115,7 @@ CREATE TABLE `categories` (
   `imagelocation` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,6 +124,7 @@ CREATE TABLE `categories` (
 
 LOCK TABLES `categories` WRITE;
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'test1',NULL),(2,'test2',NULL),(3,'test3 ',NULL),(4,'test3 ',NULL),(5,'test3 ',NULL),(6,'test3 ',NULL),(7,'test3 ',NULL),(8,'test3 ',NULL),(9,'test3 ',NULL),(10,'test3 ',NULL),(11,'test3 ',NULL),(12,'test3 ',NULL),(13,'test3 ',NULL),(14,'test3 ',NULL),(15,'test3 ',NULL),(16,'test3 ',NULL),(17,'test3 ',NULL),(18,'test3 ',NULL),(19,'test3 ',NULL),(20,'test3 ',NULL),(21,'test3 ',NULL),(22,'test3 ',NULL),(23,'test3 ',NULL),(24,'test4',NULL),(25,'test 5',NULL);
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -85,7 +142,7 @@ CREATE TABLE `statistics` (
   PRIMARY KEY (`id`),
   KEY `FK_statistics_userid` (`userid`),
   CONSTRAINT `FK_statistics_userid` FOREIGN KEY (`userid`) REFERENCES `account` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -94,7 +151,7 @@ CREATE TABLE `statistics` (
 
 LOCK TABLES `statistics` WRITE;
 /*!40000 ALTER TABLE `statistics` DISABLE KEYS */;
-INSERT INTO `statistics` VALUES (1,'2018-04-20 16:35:30',1),(2,'2018-04-20 16:35:53',1),(3,'2018-04-20 16:36:12',1),(4,'2018-04-21 10:55:38',1),(5,'2018-04-21 11:00:13',1),(6,'2018-04-21 11:58:03',1);
+INSERT INTO `statistics` VALUES (1,'2018-04-20 16:35:30',1),(2,'2018-04-20 16:35:53',1),(3,'2018-04-20 16:36:12',1),(4,'2018-04-21 10:55:38',1),(5,'2018-04-21 11:00:13',1),(6,'2018-04-21 11:58:03',1),(7,'2018-04-21 12:54:31',1);
 /*!40000 ALTER TABLE `statistics` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -107,4 +164,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-04-21 12:34:17
+-- Dump completed on 2018-04-21 14:08:28
