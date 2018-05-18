e55da9c82c092e64bd7ae85be28876196249a747
ken-b1 <ken.bauwens@student.uantwerpen.be>
2018-05-18 15:10:53 +0200

    Restapi for Boards

--- restApi/src/java/api/Board.java
+++ restApi/src/java/api/Board.java
@@ -5,44 +5,61 @@
  */
 package api;
 
+import Entities.Account;
 import configuration.setRemote;
+import java.util.List;
 import javax.json.Json;
+import javax.json.JsonArrayBuilder;
 import javax.json.JsonObjectBuilder;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import services.AccountBeanInterface;
+import services.boardCrudBeanInterface;
 /**
  *
  * @author ken
  */
-@Path("Board")
+@Path("board")
 public class Board {
     /**
     * The context to be used to perform lookups of remote beans
     */
     private static InitialContext ic;
 
-    private AccountBeanInterface account;
+    private boardCrudBeanInterface boards;
     
+    private AccountBeanInterface account;
+        
     public Board(){
         try{
             ic = new InitialContext(setRemote.setProperties());
-            System.out.println("1");
+            boards = (boardCrudBeanInterface)ic.lookup("java:global/statistics_EJB/boardCrudBean!services.boardCrudBeanInterface");
             account = (AccountBeanInterface)ic.lookup("java:global/statistics_EJB/AccountBean!services.AccountBeanInterface");
-            account.setAccount(1);
         }catch(NamingException e){
-            System.out.println("UserCategoriesAPI error:");
+            System.out.println("boardAPI error:");
             System.out.println(e.getMessage());
         }
     }
     @GET
+    @Path("{username}")
     @Produces("application/json")
-    public String getHtml() {
+    public String getUserBoards(@PathParam("username") String username) {
+        Account requestedUser = account.getAccount(username);
+        List<Entities.Board> userBoards = boards.getBoardsForUser(requestedUser);
+        
+        JsonArrayBuilder array = Json.createArrayBuilder();
         JsonObjectBuilder x = Json.createObjectBuilder();
-        x.add("test", "a");
+        
+        for(Entities.Board board: userBoards){
+            array.add(Json.createObjectBuilder().add("id", board.getId()).add("boardname", board.getBoardname()).add("isPrivate", board.getIsprivate()));
+        }
+        
+        
+        x.add("boards", array);
         return x.build().toString();
     }
 }
