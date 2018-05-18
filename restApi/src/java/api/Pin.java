7a16a0da728b903f74c140b2cbdf388f1a585d71
ken-b1 <ken.bauwens@student.uantwerpen.be>
2018-05-18 16:27:24 +0200

    restAPI for pins

--- restApi/src/java/api/Pin.java
+++ restApi/src/java/api/Pin.java
@@ -6,43 +6,75 @@
 package api;
 
 import configuration.setRemote;
+import java.util.List;
 import javax.json.Json;
+import javax.json.JsonArrayBuilder;
 import javax.json.JsonObjectBuilder;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import services.AccountBeanInterface;
+import services.boardCrudBeanInterface;
+import services.pinCrudInterface;
 /**
  *
  * @author ken
  */
-@Path("Pin")
+@Path("pin")
 public class Pin {
     /**
     * The context to be used to perform lookups of remote beans
     */
     private static InitialContext ic;
 
-    private AccountBeanInterface account;
+    private pinCrudInterface pinCrud;
+    
+    private boardCrudBeanInterface boardCrud;
     
     public Pin(){
         try{
             ic = new InitialContext(setRemote.setProperties());
-            System.out.println("1");
-            account = (AccountBeanInterface)ic.lookup("java:global/statistics_EJB/AccountBean!services.AccountBeanInterface");
-            account.setAccount(1);
+            pinCrud = (pinCrudInterface)ic.lookup("java:global/statistics_EJB/pinCrudBean!services.pinCrudInterface");
+            boardCrud = (boardCrudBeanInterface)ic.lookup("java:global/statistics_EJB/boardCrudBean!services.boardCrudBeanInterface");
         }catch(NamingException e){
-            System.out.println("UserCategoriesAPI error:");
+            System.out.println("pinAPI error:");
             System.out.println(e.getMessage());
         }
     }
     @GET
+    @Path("board/{boardid}")
     @Produces("application/json")
-    public String getHtml() {
+    public String getPinsForBoard(@PathParam("boardid") int boardId) {
+        List<Entities.Pin> pins = pinCrud.getPinsForBoard(boardCrud.getBoard(boardId));
         JsonObjectBuilder x = Json.createObjectBuilder();
-        x.add("test", "a");
+        JsonArrayBuilder array = Json.createArrayBuilder();
+        
+        for(Entities.Pin pin: pins){
+            array.add(Json.createObjectBuilder()
+                    .add("id", pin.getId())
+                    .add("name", pin.getRecipeName())
+                    .add("recipe", pin.getRecipe())
+                    .add("boardid", pin.getBoard().getId())
+                    .add("imagelocation", pin.getLocation()));
+        }
+
+        x.add("pins", array);
+        return x.build().toString();
+    }
+    
+    @GET
+    @Path("{pinid}")
+    @Produces("application/json")
+    public String getFullPin(@PathParam("pinid") int pinId) {
+        Entities.Pin fullPin = pinCrud.getPin(pinId);
+        
+        JsonObjectBuilder x = Json.createObjectBuilder();
+        x.add("id", fullPin.getId());
+        x.add("name", fullPin.getRecipeName());
+        x.add("recipe", fullPin.getRecipe());
+        x.add("location", fullPin.getLocation());
         return x.build().toString();
     }
 }
